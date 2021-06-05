973ce0b25466d0970a6c003203fea17208fe7af5
smarne <neelmarne2012@gmail.com>
2021-06-06 00:20:24 +0530

    building sidebar. sidebar header completed

---
+++ src/Sidebar.js
@@ -0,0 +1,43 @@
+// rfce - shortcut for getting component
+
+import { Avatar, IconButton } from '@material-ui/core';
+import { Chat, DonutLarge, MoreVert, SearchOutlined } from '@material-ui/icons';
+import SidebarChat from './SidebarChat';
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = () => {
+    return (
+    <div className="sidebar">
+        <div className="sidebar_header">
+            <Avatar/>
+            <div className="sidebar_headerRight">
+                <IconButton>
+                    <DonutLarge></DonutLarge>
+                </IconButton>
+                <IconButton>
+                    <Chat></Chat>
+                </IconButton>
+                <IconButton>
+                    <MoreVert></MoreVert>
+                </IconButton>
+            </div>
+        </div>
+
+        <div className="sidebar_search">
+            <div className="sidebar_searchContainer">
+                <SearchOutlined></SearchOutlined>
+                <input placeholder="Search" type="text"/>
+            </div>
+        </div>
+
+        <div className="sidebar_chats">
+            <SidebarChat />
+            <SidebarChat />
+            <SidebarChat />
+        </div>
+    </div>
+    )
+};
+
+export default Sidebar;
